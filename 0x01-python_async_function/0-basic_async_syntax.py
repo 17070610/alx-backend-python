db79134013176758e9f6f517d6a8548aea8f6c98
17070610 <schimea@hotmail.com>
2025-04-28 14:12:43 +0200

    Add the shebang

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
+"""
+    Task 0 module
+"""
 import asyncio
 import random
+
 
 async def wait_random(max_delay: int = 10) -> float:
     """
         Coroutine that waits for a random delay between 0 and max_delay seconds
-        @param max_delay: Maximum delay in seconds (default is 10)
-        :return: the actual delay time in float
     """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
